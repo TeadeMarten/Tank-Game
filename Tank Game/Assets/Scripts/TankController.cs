17de01d3d90496454c35c0283800485b2f1c9185
TeadeMarten <tmkoers@st.noorderpoort.nl>
2022-09-30 10:36:58 +0200

    addded tank controller

--- Tank Game/Assets/Scripts/TankController.cs
+++ Tank Game/Assets/Scripts/TankController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
+    [SerializeField]
+    Transform barrelRotator;
+    [SerializeField]
+    Transform firePoint;
+    [SerializeField]
+    GameObject bulletToFire;
+    public float MoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        barrelRotator.RotateAround(Vector3.forward, Input.GetAxis("Vertical") * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject b = Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            b.GetComponent<Rigidbody2D>().AddForce(barrelRotator.up * 10, ForceMode2D.Impulse);
+        }
+
+        transform.Translate(Vector2.right * Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime);
     }
 }
