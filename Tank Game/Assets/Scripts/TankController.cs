e6fe8e054f245f52183f087d842c4f5a6ef2e658
TeadeMarten <tmkoers@st.noorderpoort.nl>
2022-10-12 15:51:25 +0200

    Fixed turns
    Both tanks no longer move at the same time
    Animations on both tanks dont play at the same time anymore

--- Tank Game/Assets/Scripts/TankController.cs
+++ Tank Game/Assets/Scripts/TankController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TankController : MonoBehaviour
 {
@@ -19,8 +21,7 @@ public class TankController : MonoBehaviour
 
     public Material actiefMat;
     public Material inactiefMat;
-    bool isAanDeBeurt = false;
-
+    public bool isAanDeBeurt = false;
 
     // Start is called before the first frame update
     void Start()
@@ -31,16 +32,16 @@ public class TankController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PlayerNumber == 1)
+        if (PlayerNumber == 1 && isAanDeBeurt)
         {
             barrelRotator.RotateAround(Vector3.forward, Input.GetAxis("Vertical") * Time.deltaTime);
         }
-        if (PlayerNumber == 2)
+        if (PlayerNumber == 2 && isAanDeBeurt )
         {
             barrelRotator.RotateAround(Vector3.forward, Input.GetAxis("Vertical") * -Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAanDeBeurt)
         {
             GameObject b = Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
             if (PlayerNumber == 1) 
@@ -52,11 +53,16 @@ public class TankController : MonoBehaviour
                 b.GetComponent<Rigidbody2D>().AddForce(barrelRotator.right * -10, ForceMode2D.Impulse);
             }
         }
-        if(PlayerNumber == 1) { }
+        if(PlayerNumber == 1 && isAanDeBeurt)
+        {
+            transform.Translate(Vector2.right * Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime);
+        }
+        if (PlayerNumber ==2 && isAanDeBeurt)
+        {
+            transform.Translate(Vector2.right * Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime);
+        }
 
-        transform.Translate(Vector2.right * Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isAanDeBeurt == true)
+        if (Input.GetKeyDown(KeyCode.Space) && isAanDeBeurt)
         {
             Invoke("WisselBeurt", 0.1f);
         }
@@ -68,7 +74,7 @@ public class TankController : MonoBehaviour
 
     public void ZetActief(bool b)
     {
-        if (b == true)
+        if (b)
         {
             isAanDeBeurt = true;
             GetComponent<SpriteRenderer>().material.color = Color.red;
